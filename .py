f484f73532584ded843a7d4f0331abb2d170b99e
weissok <114381760+weissok@users.noreply.github.com>
2022-09-26 11:47:29 +0500

    Add files via upload

---
+++ .py
@@ -0,0 +1,5 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World")
+Hello, World
+>>> 
