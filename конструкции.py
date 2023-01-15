692a0f9945b30d346630765a6e11428cc02a398b
weissok <114381760+weissok@users.noreply.github.com>
2023-01-15 19:33:01 +0700

    Delete конструкции.py

--- конструкции.py
+++
@@ -1,9 +0,0 @@
-with open("1.txt") as f:
-    print(f.readline())
-    print(f.readlines())
-    print([x for x in f])
-
-with open("2.txt") as s:
-    print(s.readline())
-    print(s.readlines())
-    print([x for x in s])
